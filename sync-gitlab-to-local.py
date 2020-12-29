7e31cd6ed1cab644ddf10b465e7ae52c129aa7cd
尹彬 <1052921694@qq.com>
2020-12-29 15:28:16 +0800

    ok

--- sync-gitlab-to-local.py
+++ sync-gitlab-to-local.py
@@ -5,10 +5,9 @@
 from lib.batch_clone_from_gitlab import get_all_projects_from_gitlab, batch_clone_from_gitlab
 from lib.batch_push_to_github import batch_push_to_github
 
-gitlab_addr = '192.168.3.10:30000'
-gitlab_token = 'ymsSzkQmX3Y_14KyyBJ6'
-github_token = '5737a614b1677bbddb4b6a244a77e721d0214fc2'
-
+gitlab_addr = 'ip:port'
+gitlab_token = 'ymsSzk' # 见gitlab官网或你的gitlab私服
+github_token = '5737a6' # 见你的github
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     batch_clone_from_gitlab(gitlab_addr, gitlab_token)
